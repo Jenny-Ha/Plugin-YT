aee0dd540dae0f1c5f6ba0af5bf3945fd2b60b68
Jenny-Ha <jennyha069@gmail.com>
2017-10-29 16:19:34 -0500

    Adición de titulo a cada video

--- js/youtube.plugin.js
+++ js/youtube.plugin.js
@@ -4,13 +4,20 @@ $.fn.youtube = function(listaVideo, width="100%", height)
 	{
 		for (var i = 0; i < listaVideo.length; i++)
 		{
+			/* Creando Iframe del video */
 			let contVideo = '<iframe class="videoYT" frameborder="0" allowfullscreen></iframe>';
 			let URL = 'https://www.youtube.com/embed/' + listaVideo[i].url;
 			console.log(URL);
 			let $cont_video = $(contVideo);
+			
+
+			/* Titulo para el video */
+			let $caption = listaVideo[i].title;
+
 			$cont_video.attr('src', URL);
 			$cont_video.css('width', width);
 			$cont_video.css('height', height)
+			$(this).append('<h3 class="videoTitle">' + $caption + '</h3>');	
 			$(this).append($cont_video);			
 		};
 	}
